2b23017bd34168d187b9290af2da7e5ef8668489
Luis Ferreira <apenaspara1998@gmail.com>
2019-12-07 21:24:06 +0000

    Reverting

--- graphics/canvas.js
+++ graphics/canvas.js
@@ -113,10 +113,8 @@ class Painter {
      * @param {[number,number]} point1 
      * @param {[number,number]} point2 
      */
-    fillRect(point1, point2) {
-        let [x1,y1] = point1;
-        let [x2,y2] = point2;
-        this.ctx.fillRect(x1, y1, x2 - x1, y2 - y1);
+    rect(point1, point2) {
+        this.ctx.fillRect(point1.x, point1.y, point2.x - point1.x, point2.y - point1.y);
     }
 
     /**
@@ -135,7 +133,7 @@ class Painter {
      * @param {[number,number]} center 
      * @param {number} radius 
      */
-    strokeCircle(center, radius) {
+    circle(center, radius) {
         this.ctx.beginPath();
         this.ctx.arc(center[0], center[1], radius, 0, Math.PI * 2);
         this.ctx.stroke();
